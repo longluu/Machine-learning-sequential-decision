59ec0e71aa278ad0aa2961404220cb3d7f85b512
Long T Luu <longluu@sas.upenn.edu>
2020-06-03 11:46:20 -0400

    Update plot llh code

--- Working_memory_feedback/Experiment 2/Analysis and Plot model/plot_llhRatio.m
+++ Working_memory_feedback/Experiment 2/Analysis and Plot model/plot_llhRatio.m
@@ -4,13 +4,15 @@ neg_llh_noResample = [4539.91 5901.30 6051.89 6413.41 6429.30 30138.46 ...
                   
 neg_llh_Resample =   [4533.30 5887.88 6039.95 6409.87 6405.35 30131.41 ...
                       4820.28 5054.09 4921.72 4398.48 4400.77 5534.17 5134.66 38872.65];      
-                  
+ratio_llh =  neg_llh_noResample ./ neg_llh_Resample - 1;                 
 figure
 hold on
-bar(1:length(neg_llh_Resample), neg_llh_noResample ./ neg_llh_Resample)
-plot(1:length(neg_llh_Resample), ones(1,length(neg_llh_Resample)), 'k--')
+bar(1:length(neg_llh_Resample), ratio_llh)
 set(gca, 'XTick', 1:length(neg_llh_Resample), ...
-            'XTickLabel', num2cell(1:length(neg_llh_Resample)))
-ylim([0.99 1.011])
+            'XTickLabel', num2cell(1:length(neg_llh_Resample)), ...
+            'YTick', [-0.005 0 0.005 0.01], ...
+            'YTickLabel', num2cell([-0.005 0 0.005 0.01]+1))
+        
+ylim([-0.005 0.01])
 xlabel('Subject')
 ylabel('Ratio -LLH no-resample vs. resample')
