203cc4c7b27a400f8cccaba5f3a8135f697d557e
Long T Luu <thelong20.4@gmail.com>
2020-12-22 13:59:11 -0500

    Update fit with lapse

--- Working_memory_feedback/Experiment 2/Model fit/Fit routine/runModelFit.m
+++ Working_memory_feedback/Experiment 2/Model fit/Fit routine/runModelFit.m
@@ -1,6 +1,6 @@
 %%%%%%%%%%%%% Run the full model fitting program %%%%%%%%%%%%%
 clear 
-nLoops = 50;
+nLoops = 30;
 negLLHMinLoop = [];
 fitParameterAll = cell(nLoops, 1);
 negLLH = NaN(nLoops, 1);
@@ -18,30 +18,28 @@ negLLH = NaN(nLoops, 1);
 % Use binned or unbinned likelihood: 0: unbinned
 %                                    1: binned
 modelType = 1;
-useResampling = 1; % only for self-consistent Bayes
+useResampling = 0; % only for self-consistent Bayes
 optimizationAlgorithm = 1;
 SetStartPoint = 0;
-% initialValueAll = [4.3425    6.2248           0.0000     19.1377    -9.6045   3.5283    2.0902    0.9927    0.6813;
-%                 8.7205    8.8878           0.0000     33.0312   -21.2586   1.2076    1.8928    0.9215    0.4348;
-%                 8.3099    8.7268           0.0000     13.9033   -12.6251   0.6127    2.7094    0.9468    0.5099;
-%                 6.3379    8.4823           0.0000     19.3091   -12.7690   0.9699    2.6041    0.5558    0.4201;
-%                 6.4379    9.9076           0.0000     32.5355   -17.6389   3.0964    1.5830    0.2067    0.4086;
-%                 9.7284   15.1847           0.0000     56.3034   -42.2707   0.4378    4.0136    0.9881    0.5523;
-%                 7.8510    9.8641           0.0000     22.8922   -18.0641   0.8543    3.9069    0.9646    0.4949;
-%                 6.0243    8.0650           0.0000     32.4649   -19.9032   5.3989    2.4021    0.9986    0.5303];
+initialValueAll = [4.3425    6.2248           0.0000     19.1377    -9.6045   3.5283    2.0902    0.9927    0.6813;
+                8.7205    8.8878           0.0000     33.0312   -21.2586   1.2076    1.8928    0.9215    0.4348;
+                8.3099    8.7268           0.0000     13.9033   -12.6251   0.6127    2.7094    0.9468    0.5099;
+                6.3379    8.4823           0.0000     19.3091   -12.7690   0.9699    2.6041    0.5558    0.4201;
+                6.4379    9.9076           0.0000     32.5355   -17.6389   3.0964    1.5830    0.2067    0.4086;
+                9.7284   15.1847           0.0000     56.3034   -42.2707   0.4378    4.0136    0.9881    0.5523;
+                7.8510    9.8641           0.0000     22.8922   -18.0641   0.8543    3.9069    0.9646    0.4949;
+                6.0243    8.0650           0.0000     32.4649   -19.9032   5.3989    2.4021    0.9986    0.5303];
                 %[stdNoise                  lapseRate  priorRange   stdMem  stdMotor smoothPrior    pCW]
-                
-initialValueAll = [6.0243    8.0650           0.0000     32.4649   -19.9032   5.3989    2.4021    0.9986    0.5303];
-                
+                                
 binnedLH = 0;
 plotFitProgress = 0;
 plotEstFit = 0;
-subjectID = {'average'}; % 'll', 'pw', 'eh', 'bh', 'ln', 'at', 'dh', 
+subjectID = {'ll', 'pw', 'eh', 'bh', 'ln', 'at', 'dh'}; % 'll', 'pw', 'eh', 'bh', 'ln', 'at', 'dh', 'average'
 expNumber = 1;
 includeIncongruentTrials = 0; 
 correctTrialOnly = 1; 
 fixMotorNoise = 0;
-fixLapseRate = 1;
+fixLapseRate = 0;
 fixSmoothPrior = 0;
 fixPcw = 0;
 
